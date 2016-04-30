6b8b9230e50b46af91d49892fe90067940956c26
Trevor Gau <trgau@microsoft.com>
2016-04-29 22:41:37 -0400

    Body parser options

--- koa-bodyparser/koa-bodyparser.d.ts
+++ koa-bodyparser/koa-bodyparser.d.ts
@@ -1,11 +1,51 @@
 import koa = require("koa");
 
+/**
+ * Augment the koa Request to include the parsed body
+ */
 declare module "koa" {
 	interface Request {
-		body: string;
+		body: any;
 	}
 }
 
-declare var bodyParser: () => any;
+interface BodyParserOptions {
+	/**
+	 * Requested encoding. Default is utf-8
+	 */
+	encode: string;
+
+	/**
+	 * limit of the urlencoded body. If the body ends up being larger, a 413 error is returned. Default is 56kb
+	 */
+	formLimit: string;
+
+	/**
+	 * limit of the json body. Default is 1mb.
+	 */
+	jsonLimit: string;
+
+	/**
+	 * when set to true, JSON parser will only accept arrays and objects. Default is true.
+	 */
+	strict: boolean;
+
+	/**
+	 * custom json request detection function. Default is null.
+	 */
+	detectJSON: (ctx: koa.Context) => boolean;
+
+	/**
+	 * Support additional content types.
+	 */
+	extendTypes: {[type: string]: string[]};
+
+	/**
+	 * custom error handler.
+	 */
+	onerror: (err: any, ctx: koa.Context) => void;
+}
+
+declare var bodyParser: (options?: BodyParserOptions) => (ctx: koa.Context, next: Function) => any;
 
 export = bodyParser;
